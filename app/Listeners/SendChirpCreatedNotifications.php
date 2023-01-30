8e0c1e92aa2b59d725c78842dc1c10c59084e221
iradawn1204 <iradawn04@gmail.com>
2023-01-31 00:03:16 +0800

    Chirper Project

---
+++ app/Listeners/SendChirpCreatedNotifications.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\ChirpCreated;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendChirpCreatedNotifications implements ShouldQueue
+{
+    
+    public function __construct()
+    {
+        //
+    }
+
+   
+    public function handle(ChirpCreated $event)
+    {
+        foreach (User::whereNot('id', $event->chirp->user_id)->cursor() as $user) {
+            $user->notify(new NewChirp($event->chirp));
+        }
+    }
+}
